7bedae503443f154f6d900d9bd4f15ce930e11ee
HWChia <qaz431220@gmail.com>
2022-05-11 15:46:44 +0800

    ** 05/11 **
    ** -- Menu -- **
    1: 從外部匯入照片
     > Resources
     > SteeamingAssets
    2: 程式控制拉桿控制音量
    3: 程式控制 Toggle
     > BGM
     > SFX

--- SpaceShooter/Assets/1_Scripts/scr_Menumanager.cs
+++ SpaceShooter/Assets/1_Scripts/scr_Menumanager.cs
@@ -1,25 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using System.IO;
 
 public class scr_Menumanager : MonoBehaviour
 {
     #region - Variables -
     [SerializeField] [Header("設定畫面")] GameObject settingPage;
-    [SerializeField] [Header("聲音圖片")] Sprite[] sound_sprite;
-    [SerializeField] [Header("靜音按鈕 - 圖片")] Image mute_img;
+    [SerializeField] [Header("開聲音 - 圖片")] Sprite soundOn_sprite;
+    [SerializeField] [Header("關聲音 - 圖片")] Sprite soundOff_sprite;
+    [SerializeField] [Header("靜音按鈕 - 物件")] Image mute_img;
     [SerializeField] [Header("聲音拉桿")] Slider sound_Slider;
-
-    Sprite soundOpenSprite;
+    [SerializeField] [Header("Audio Mixer")] AudioMixer am;
+    [SerializeField] [Header("BGM Toggle")] Toggle bgm_Toggle;
+    [SerializeField] [Header("SFX Toggle")] Toggle sfx_Toggle;
 
     bool isSetting;              // 是否開啟設定畫面
     bool isMute;                 // 是否全部靜音
 
     Button start_btn;            // 開始按鈕
 
-    [SerializeField] [Header("Stream Assets 路徑")] string Path;
-    [SerializeField] [Header("Stream Assets 路徑內檔案")] string[] FilePath;
+    [SerializeField] [Header("Stream Assets 路徑")] string path;
+    [SerializeField] [Header("Stream Assets 路徑內檔案")] string[] fileInPath;
     #endregion
 
     #region - MonoBehaviours - 
@@ -31,8 +34,8 @@ public class scr_Menumanager : MonoBehaviour
         LoadStreamFile();
 
         // 使用 Resources
-        sound_sprite[0] = Resources.Load<Sprite>("Sound on");
-        sound_sprite[1] = Resources.Load<Sprite>("Sound off");
+        soundOn_sprite = Resources.Load<Sprite>("Sound on");
+        soundOff_sprite = Resources.Load<Sprite>("Sound off");
 
         // 使用 StreamingAssets
     }
@@ -66,8 +69,8 @@ public class scr_Menumanager : MonoBehaviour
     {
         isMute = !isMute;
 
-        if (isMute) mute_img.sprite = sound_sprite[1];
-        else if (!isMute) mute_img.sprite = sound_sprite[0];
+        if (isMute) mute_img.sprite = soundOff_sprite;
+        else if (!isMute) mute_img.sprite = soundOn_sprite;
 
         // pause = true > 靜音  |  pause = false > 恢復聲音
         AudioListener.pause = isMute;
@@ -91,6 +94,16 @@ public class scr_Menumanager : MonoBehaviour
     }
 
     /// <summary>
+    /// 控制切換
+    /// </summary>
+    public void ControlToggle(string audiomixer)
+    {
+        if (bgm_Toggle.isOn) am.SetFloat(audiomixer, 0f);
+
+        else am.SetFloat(audiomixer, -80f);
+    }
+
+    /// <summary>
     /// 按鈕點擊
     /// </summary>
     void ButtonEvent()
@@ -107,8 +120,6 @@ public class scr_Menumanager : MonoBehaviour
         isMute = false;
 
         sound_Slider.value = 0.5f;
-
-        sound_sprite = new Sprite[2];
     }
 
     /// <summary>
@@ -117,17 +128,17 @@ public class scr_Menumanager : MonoBehaviour
     void LoadStreamFile()
     {
         // 讀取 StreamingAssets 路徑
-        Path = Application.streamingAssetsPath;
+        path = Application.streamingAssetsPath;
         // 抓取 StreamingAssets 資料夾內所有的 png 檔
-        FilePath = Directory.GetFiles(Path, "*.png");
+        fileInPath = Directory.GetFiles(path, "*.png");
         // 讀取 StreamingAssets 資料夾內所有的 png 圖檔並轉換成 Byte，使程式看的懂得格式
-        byte[] pngBytes = System.IO.File.ReadAllBytes(FilePath[0]);
+        byte[] pngBytes = System.IO.File.ReadAllBytes(fileInPath[0]);
         // 定義圖片大小
         Texture2D tex = new Texture2D(2, 2);
         // 讀取圖片
         tex.LoadImage(pngBytes);
         // 將圖片轉換成 sprite
-        soundOpenSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0f, 0f), 100f);
+        soundOn_sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0f, 0f), 100f);
     }
 
     /// <summary>
